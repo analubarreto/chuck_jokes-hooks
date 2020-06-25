a87a3f6530f2f0e5a75f1c1fd15da8b35a9125a4
analubarreto <analuibm@gmail.com>
2020-06-25 19:51:52 -0300

    finished search bar styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid } from 'semantic-ui-react';
+
+// Custom components
+import chuck from '../features/api/apiCall';
+import { SearchBar } from '../features/SearchBar';
+import { JokeList } from '../jokes/JokeList';
 
 const App = () => {
+  const [jokes, setJokes] = useState(null)
+
   return (
-    <div className="App">
-      <h1>App Home</h1>
-    </div>
+    <Grid container className="App">
+      <SearchBar />
+    </Grid>
   );
 }
 
--- src/features/SearchBar.js
+++ src/features/SearchBar.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Container } from 'semantic-ui-react'
 
-export const SearchBar = () => {
+export const SearchBar = ({onSubmit}) => {
+  const [term, setTerm] = useState()
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+
+    onSubmit(term)
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Form onSubmit={onFormSubmit}>
+        <Form.Field>
+          <label>
+            Joke Search:
+            <input
+              type="text"
+              placeholder="Joke's theme"
+            />
+          </label>
+        </Form.Field>
+      </Form>
+    </Container>
   )
 }
